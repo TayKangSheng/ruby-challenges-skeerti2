ef8631fe739b200ccdbf147822343fc9a54e6d3d
Sruti Keerti <skeerti2@gmail.com>
2017-02-28 10:17:03 +0800

    factorial question completed

--- lib/05factorial.rb
+++ lib/05factorial.rb
@@ -9,3 +9,19 @@
 #
 # > 120
 #
+#every recursion needs two things: Base case (when to stop) and recursion case (what happens if it doesnt stop)
+
+# 5! = 5*4*3*2*1
+#Base case : stops at 1
+#Recursion Case: f(5) = 5* f(4)
+                  #f(4) = 4*f(3)
+
+def factorial(n)
+  if(n==0)
+    1
+  else
+    n*factorial(n-1)
+  end
+  end
+
+  p factorial(3)
